769ba4112ae052c6a0618e2734fa0e1cb470682b
Samanimkr <samanimkr@outlook.com>
2018-10-25 09:48:11 +0100

    fixed scrollview issue

--- App.js
+++ App.js
@@ -8,7 +8,7 @@
 
 import React, {Component} from 'react';
 import axios from 'axios';
-import {StyleSheet, Text, View, Image} from 'react-native';
+import {StyleSheet, Text, View, Image ,ScrollView} from 'react-native';
 
 
 
@@ -17,7 +17,9 @@ export default class App extends Component {
     super();
 
     this.state = {
-      image_uri: ''
+      earth: [],
+      mars: [],
+      saturn: []
     }
   }
 
@@ -25,6 +27,7 @@ export default class App extends Component {
     axios
     .get(`https://images-api.nasa.gov/search?q=mars&media_type=image`)
     .then(response => {
+      console.log('response: ', response);
       var nasa_id = response.data.collection.items[6].data[0].nasa_id;
       this.setState({
         image_uri: `https://images-assets.nasa.gov/image/${nasa_id}/${nasa_id}~orig.jpg`
@@ -32,9 +35,6 @@ export default class App extends Component {
 
       console.log('nasa_id: ', nasa_id);
       // console.log('DATA: ', response.data.collection.items[1])
-
-      // return <Image source={nasa_image} style={styles.img} />
-      // return `https://images-assets.nasa.gov/image/${nasa_id}/${nasa_id}~orig.jpg`;
     })
     .catch(e => console.log('ERROR: ', e))
   }
@@ -46,12 +46,20 @@ export default class App extends Component {
     // setTimeout(() => {
     //   console.log('imageInfo', imageInfo);
     // }, 1000);
-    this.getNasaImages();
+    if (!this.state.image_uri){
+      this.getNasaImages();
+    }
+    
 
     return (
       <View style={styles.container}>
-        <Image style={{width: '70%', height: '50%'}} source={{uri: this.state.image_uri}}/>
-        <Text style={styles.welcome}>Welcome to NASA Native!</Text>
+        <Text style={styles.title}>Welcome to NASA Native!</Text>
+        <ScrollView contentContainerStyle={styles.scrollview}>
+          <Image style={styles.img} source={{uri: this.state.image_uri}}/>
+          <Image style={styles.img} source={{uri: this.state.image_uri}}/>
+          <Image style={styles.img} source={{uri: this.state.image_uri}}/>
+          <Image style={styles.img} source={{uri: this.state.image_uri}}/>
+        </ScrollView>
       </View>
     );
   }
@@ -60,17 +68,23 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    paddingTop: 36,
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  scrollview: {
+    backgroundColor: 'red',
+    flexDirection: 'column',
+  },
+  title: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
   },
   img: {
-    width: '80%',
-    height: '40%'
+    flex: 1,
+    width: 250,
+    height: 200,
+    marginTop: 15
   }
 });
