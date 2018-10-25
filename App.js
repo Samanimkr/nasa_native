862a7c4fc5d09f6ba58300f039f74a413d0396bb
Samanimkr <samanimkr@outlook.com>
2018-10-25 11:55:35 +0100

    moved images to the new component

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import axios from 'axios';
 import { StyleSheet, Text, View, Image ,ScrollView } from 'react-native';
 
 
@@ -27,52 +26,13 @@ export default class App extends Component {
     }
   }
 
-  getNasaImages(){
-    axios
-    .get(`https://images-api.nasa.gov/search?q=mars&media_type=image`)
-    .then(response => {
-      let images_data = [...response.data.collection.items];
-      let mars_images = [], nasa_id = '', image_uri = '';
-
-      for (let index = 0; index < 10; index++) {
-        nasa_id = images_data[index].data[0].nasa_id;
-
-        mars_images.push({
-          id: nasa_id,
-          uri: `https://images-assets.nasa.gov/image/${nasa_id}/${nasa_id}~orig.jpg`,
-          description: images_data[index].data[0].description
-        });
-      }
-
-      this.setState({
-        isDataAvailable: true,
-        mars: [...mars_images]
-      });
-      
-    })
-    .catch(e => console.log('ERROR: ', e))
-  }
-
 
   render() {
-    if (!this.state.isDataAvailable){
-      this.getNasaImages();
-    } 
-
-    console.log(PlanetImages);
-    
-
+    console.log('hello');
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Welcome to NASA Native!</Text>
-        <PlanetImages welcome='HELLO!'/>
-        <ScrollView contentContainerStyle={styles.scrollview}>
-          { this.state.isDataAvailable &&
-            this.state.mars.map(image => {
-              return <Image key={image.id} style={styles.img} source={{uri: image.uri}}/>;
-            })
-          }
-        </ScrollView>
+        <PlanetImages planet='earth'/>
       </View>
     );
   }
--- components/PlanetImages.js
+++ components/PlanetImages.js
@@ -2,10 +2,72 @@ import React, {Component} from 'react';
 import { StyleSheet, Text, View, Image ,ScrollView } from 'react-native';
 import axios from 'axios';
 
-export default class PlanetImages extends Component {
+export class PlanetImages extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            isDataAvailable: false,
+            images: [{
+                id: '',
+                uri: '',
+                description: ''
+            }],
+        }
+    }
+
+    getNasaImages(){
+        axios
+        .get(`https://images-api.nasa.gov/search?q=${this.props.planet}&media_type=image`)
+        .then(response => {
+            let images_data = [...response.data.collection.items];
+            let planet_images = [], nasa_id = '', image_uri = '';
+
+            for (let index = 0; index < 10; index++) {
+            nasa_id = images_data[index].data[0].nasa_id;
+
+            planet_images.push({
+                id: nasa_id,
+                uri: `https://images-assets.nasa.gov/image/${nasa_id}/${nasa_id}~orig.jpg`,
+                description: images_data[index].data[0].description
+            });
+            }
+
+            this.setState({
+                isDataAvailable: true,
+                images: [...planet_images]
+            });
+            
+        })
+        .catch(e => console.log('ERROR: ', e))
+    }
+
     render(){
+        if (!this.state.isDataAvailable){
+            this.getNasaImages();
+          } 
+
         return (
-            <h1>{this.props.welcome}</h1>
+            <ScrollView contentContainerStyle={styles.scrollview}>
+                { this.state.isDataAvailable &&
+                    this.state.images.map(image => {
+                    return <Image key={image.id} style={styles.img} source={{uri: image.uri}}/>;
+                    })
+                }
+            </ScrollView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    scrollview: {
+      flexDirection: 'column',
+    },
+    img: {
+      flex: 1,
+      width: 350,
+      height: 260,
+      marginTop: 15
+    }
+  });
+  
