3fe4686cb80e83a5e5107fafd781570ccb1dff04
Samanimkr <samanimkr@outlook.com>
2018-10-26 16:01:21 +0100

    styled the flat list

--- components/Selector.js
+++ components/Selector.js
@@ -1,17 +1,18 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Alert, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, Alert, FlatList, TouchableHighlight } from 'react-native';
 
 export default class ImageCard extends Component {
   constructor(props){
     super(props);
 
     this.state = {
-      selectedText: this.props.defaultValue
+      selectedText: this.props.defaultValue,
+      displayMenu: 'none'
     }
   }
 
   _onPressButton(){
-    Alert.alert('HELLO THERE!')
+    
   }
 
   render(){
@@ -20,6 +21,15 @@ export default class ImageCard extends Component {
         <TouchableHighlight style={styles.container} onPress={this._onPressButton}>
           <Text style={styles.buttonText}>{this.state.selectedText}</Text>
         </TouchableHighlight>
+
+        <FlatList
+          style={styles.flatlist}
+          data={[...this.props.options]}
+          renderItem={({item}) => (
+            <Text style={[styles.flatlistText, this.state.selectedText===item && {backgroundColor: '#eee'}]}>{item}</Text>
+          )}
+        />
+
       </View>
       
     )
@@ -30,6 +40,24 @@ export default class ImageCard extends Component {
 const styles = StyleSheet.create({
   container: {
     marginBottom: 15,
+  },
+  flatlist: {
+    display: this.state.displayMenu,
+    position: 'relative',
+    bottom: 15,
+    left: 0,
+    height: 20,
+    overflow: 'scroll',
+    flex: 0
+  },
+  flatlistText: {
+    fontSize: 18, 
+    color: '#444',
+    padding: 5,
+    borderWidth: 2,
+    borderTopWidth: 0,
+    borderColor: '#eee',
+    flex: 1,
   },
   buttonText: {
     fontSize: 20,
